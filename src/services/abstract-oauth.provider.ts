95410affe197c311a52473734c80f1f642d9efa9
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-04-30 20:28:33 -0400

    Implement base OAuth provider

---
+++ src/services/abstract-oauth.provider.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
+import { Observable, from, map, of, switchMap, take } from 'rxjs';
+import { UserProfile } from '../models/user-profile';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class AbstractOAuthProvider implements AuthenticationService {
+
+  constructor(protected readonly oAuthService: OAuthService) {
+    this.oAuthService.configure(this.getAuthConfig());
+  }
+
+  logIn(): Observable<UserProfile> {
+    return from(this.oAuthService.loadDiscoveryDocument()).pipe(
+      take(1),
+      switchMap(() => from(this.oAuthService.tryLoginImplicitFlow())),
+      switchMap(() => {
+        if (!this.oAuthService.hasValidAccessToken() || this.tokenExpired()) {
+          this.oAuthService.initLoginFlow(undefined, this.getInitFlowParams());
+          return of(null);
+        }
+        return from(this.oAuthService.loadUserProfile());
+      }),
+      map((result) => result as UserProfile));
+  }
+
+  logOut() {
+    // For Keycloak, for some reason, logoutUrl is lost on oAuthService and requires 
+    // reconfiguration before logging out.
+    this.oAuthService.configure(this.getAuthConfig());
+    this.oAuthService.logOut();
+  }
+
+  protected abstract getAuthConfig(): AuthConfig;
+
+  protected getInitFlowParams() {
+    return {};
+  }
+
+  protected tokenExpired() {
+    return Date.now() - this.oAuthService.getIdTokenExpiration() > 0;
+  }
+
+}
--- src/services/keycloak-api.service.ts
+++ src/services/keycloak-api.service.ts
@@ -1,51 +1,27 @@
 import { Injectable } from '@angular/core';
 import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
-import { Observable, from, map, of, switchMap, take } from 'rxjs';
-import { UserProfile } from '../models/user-profile';
-import { AuthenticationService } from './authentication.service';
-
-const authConfig: AuthConfig = {
-  issuer: 'http://192.168.2.12:2080/realms/ostock-realm',
-  strictDiscoveryDocumentValidation: false,
-  redirectUri: document.location.origin,
-  clientId: 'ostock',
-  scope: 'openid profile email',
-  requireHttps: false, // TODO: Change this before going live
-  loginUrl: 'http://192.168.2.12:2080',
-  logoutUrl: document.location.origin + '/logout'
-}
+import { AbstractOAuthProvider } from './abstract-oauth.provider';
 
 @Injectable({
   providedIn: 'root'
 })
-export class KeycloakApiService implements AuthenticationService {
+export class KeycloakApiService extends AbstractOAuthProvider {
 
-  constructor(private readonly oAuthService: OAuthService) {
-    this.oAuthService.configure(authConfig);
+  constructor(protected override readonly oAuthService: OAuthService) {
+    super(oAuthService);
   }
 
-  logIn(): Observable<UserProfile> {
-    return from(this.oAuthService.loadDiscoveryDocument()).pipe(
-      take(1),
-      switchMap(() => from(this.oAuthService.tryLoginImplicitFlow())),
-      switchMap(() => {
-        if (!this.oAuthService.hasValidAccessToken() || this.tokenExpired()) {
-          this.oAuthService.initLoginFlow();
-          return of(null);
-        }
-        return from(this.oAuthService.loadUserProfile());
-      }),
-      map((result) => result as UserProfile));
-  }
-
-  logOut() {
-    // For some reason, logoutUrl is lost on oAuthService and requires reconfiguration before logging out.
-    this.oAuthService.configure(authConfig);
-    this.oAuthService.logOut();
-  }
-
-  private tokenExpired() {
-    return Date.now() - this.oAuthService.getIdTokenExpiration() > 0;
+  protected override getAuthConfig(): AuthConfig {
+    return {
+      issuer: 'http://192.168.2.12:2080/realms/ostock-realm',
+      strictDiscoveryDocumentValidation: false,
+      redirectUri: document.location.origin,
+      clientId: 'ostock',
+      scope: 'openid profile email',
+      requireHttps: false, // TODO: Change this before going live
+      loginUrl: 'http://192.168.2.12:2080',
+      logoutUrl: document.location.origin + '/logout'
+    };
   }
 
 }
