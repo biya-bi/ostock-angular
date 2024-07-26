4cbb372e499dfd768f7b4da125fa8270be9704e8
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-07-26 15:08:46 -0400

    Update Keycloak provider

--- src/providers/keycloak-oauth.provider.ts
+++ src/providers/keycloak-oauth.provider.ts
@@ -13,13 +13,13 @@ export class KeycloakOAuthProvider extends AbstractOAuthProvider {
 
   protected override getAuthConfig(): AuthConfig {
     return {
-      issuer: 'http://192.168.2.12:2080/realms/ostock-realm',
+      issuer: 'http://keycloak.infra:8080/realms/ostock',
       strictDiscoveryDocumentValidation: false,
       redirectUri: document.location.origin,
       clientId: 'ostock',
       scope: 'openid profile email',
       requireHttps: false, // TODO: Change this before going live
-      loginUrl: 'http://192.168.2.12:2080',
+      loginUrl: 'http://keycloak.infra:8080/realms/ostock',
       logoutUrl: document.location.origin + '/logout'
     };
   }
